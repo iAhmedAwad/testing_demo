212fb48b3a00b5d3ffa10ddd481e240d9db271c9
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-06 09:14:42 +0200

    Added more test methods

--- app/src/test/java/com/trianglz/testing_demo/RegistrationUtilTest.kt
+++ app/src/test/java/com/trianglz/testing_demo/RegistrationUtilTest.kt
@@ -6,6 +6,74 @@ import org.junit.Test
 
 class RegistrationUtilTest {
 
+    //Naming convention should follow (MethodName_StateUnderTest_ExpectedBehavior)
+    @Test
+    fun validateRegistration_emptyUserName_returnsFalse() {
+        // Given is username, password, confirmPassword
 
+        val result = RegistrationUtil.validateRegistration(
+            "", "123456aF",
+            "123456aF"
+        )
+
+        //assertEquals(result, false)
+        assertThat(result).isFalse()
+
+        //assertThat("some text").
+
+    }
+
+    @Test
+    fun validateRegistration_validUsernameAndRepeatedPassword_returnsTrue() {
+        // Given is username, password, confirmPassword
+
+        val result = RegistrationUtil.validateRegistration(
+            "pony", "123456aF",
+            "123456aF"
+        )
+
+        assertThat(result).isTrue()
+
+    }
+
+    @Test
+    fun validateRegistration_usernameExists_returnsFalse() {
+        // Given is username, password, confirmPassword
+
+        val result = RegistrationUtil.validateRegistration(
+            "Doe", "123456aF",
+            "123456aF"
+        )
+
+
+        assertThat(result).isFalse()
+
+    }
+
+    @Test
+    fun validateRegistration_emptyPassword_returnsFalse() {
+        // Given is username, password, confirmPassword
+
+        val result = RegistrationUtil.validateRegistration(
+            "Doe", "",
+            "123"
+        )
+
+        assertThat(result).isFalse()
+
+    }
+
+    @Test
+    fun validateRegistration_incorrectlyConfirmedPassword_returnsFalse() {
+        // Given is username, password, confirmPassword
+
+        val result = RegistrationUtil.validateRegistration(
+            "pony", "324",
+            "123"
+        )
+
+        assertThat(result).isFalse()
+
+    }
 
 }
