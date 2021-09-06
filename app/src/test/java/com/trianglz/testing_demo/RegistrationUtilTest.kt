400a481e50847ed88740379f0ff3a07b2229caa7
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-06 09:13:07 +0200

    Update RegistrationUtilTest.kt

--- app/src/test/java/com/trianglz/testing_demo/RegistrationUtilTest.kt
+++ app/src/test/java/com/trianglz/testing_demo/RegistrationUtilTest.kt
@@ -6,22 +6,6 @@ import org.junit.Test
 
 class RegistrationUtilTest {
 
-    //Naming convention should follow (MethodName_StateUnderTest_ExpectedBehavior)
-    @Test
-    fun validateRegistration_emptyUserName_returnsFalse() {
-        // Given is username, password, confirmPassword
-
-        val result = RegistrationUtil.validateRegistration(
-            "", "123456aF",
-            "123456aF"
-        )
-
-        //assertEquals(result, false)
-        assertThat(result).isFalse()
-
-        //assertThat("some text").
-
-    }
 
 
 }
