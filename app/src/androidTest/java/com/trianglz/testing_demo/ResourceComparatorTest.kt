7c60d99c4b9f9927c5feeeda22d95de858d72669
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-06 10:15:29 +0200

    Update ResourceComparatorTest.kt

--- app/src/androidTest/java/com/trianglz/testing_demo/ResourceComparatorTest.kt
+++ app/src/androidTest/java/com/trianglz/testing_demo/ResourceComparatorTest.kt
@@ -1,14 +1,31 @@
 package com.trianglz.testing_demo
 
 import android.content.Context
+import android.util.Log
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 
 class ResourceComparatorTest{
 
-    private val resourceCompare = ResourceComparator()
+    //Bad practice .. what if there is a counter or whatever? every test case will change that counter
+    //private val resourceCompare = ResourceComparator()
+    private lateinit var resourceCompare : ResourceComparator
+
+    @Before
+    fun setup(){
+        resourceCompare = ResourceComparator()
+        Log.d(TAG, "setup: setting up...")
+    }
+
+    @After
+    fun eliminate(){
+        //close database or whatever is necessary after finishing each and every testcase...
+        Log.d(TAG, "eliminate: Eliminating...")
+    }
 
     @Test
     fun isEqual_sameAsGivenString_returnsTrue(){
@@ -29,5 +46,8 @@ class ResourceComparatorTest{
         val result = resourceCompare.isEqual(context, R.string.app_name, "meh")
 
         assertThat(result).isFalse()
+    }
+    companion object{
+        private const val TAG = "ResourceComparatorTest"
     }
 }
